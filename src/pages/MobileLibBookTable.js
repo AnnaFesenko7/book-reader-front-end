7478f270649c4eb84ae7763739cace7930d4aecd
AnnaFesenko7 <annafecenko7@gmail.com>
2023-05-16 21:43:22 +0300

    fix-button

--- src/pages/MobileLibBookTable.js
+++ src/pages/MobileLibBookTable.js
@@ -1,12 +1,13 @@
 import { LibBookTable } from 'components/LibBookTable/LibBookTable';
 import { MobileLinkToForm } from 'components/MobileLinkToForm/MobileLinkToForm';
 import { StyledContainer } from 'components/StyledContainer/StyledContainer.styled';
-
+import { useMediaQuery } from 'react-responsive';
 export const MobileLibBookTable = () => {
+  const isMobileDevice = useMediaQuery({ query: '(max-width: 767px)' });
   return (
     <StyledContainer>
       <LibBookTable />
-      <MobileLinkToForm to="/" />
+      {isMobileDevice && <MobileLinkToForm to="/" />}
     </StyledContainer>
   );
 };
